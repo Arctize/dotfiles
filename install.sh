04c570555c2513376b7506558a26890dd7b3d384
Arctize <patrick.wicki96@gmail.com>
2016-06-26 20:15:25 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ ln -s ~/dotfiles/.xinitrc ~/
 ln -s ~/dotfiles/.vim ~/
 ln -s ~/dotfiles/.vimrc ~/
 
-mkdir -p ~/.config
-ln -s ~/dotfiles/.config/i3 ~/.config/
-ln -s ~/dotfiles/.config/ranger ~/.config/ranger
-ln -s ~/dotfiles/.config/sxhkd ~/.config/sxhkd
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/ranger
+mkdir -p ~/.config/sxhkd
+ln -s ~/dotfiles/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+ln -s ~/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -s ~/dotfiles/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
