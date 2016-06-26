7ba05ad8f78fc8e9c0c0a2f6c0dd0a915b8cc1b6
Arctize <patrick.wicki96@gmail.com>
2016-06-26 22:24:46 +0200

    added qutebrowser configs

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ ln -si ~/.dotfiles/.vimrc ~/
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/sxhkd
+
 ln -si ~/.dotfiles/i3/config ~/.config/i3/config
 ln -si ~/.dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -si ~/.dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -si ~/.dotfiles/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -si ~/.dotfiles/qutebrowser ~/.config/qutebrowser
