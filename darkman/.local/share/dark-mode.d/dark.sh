5c3f06c76d0159f8157622621a98c4bf4d37531d
arctize <patrick.wicki96@gmail.com>
2022-05-05 20:12:21 +0200

    waybar: notify module on darkmode toggle

--- darkman/.local/share/dark-mode.d/dark.sh
+++ darkman/.local/share/dark-mode.d/dark.sh
@@ -7,5 +7,8 @@ sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "Default Dark+
 # GTK
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 
+# notify waybar module
+pkill -RTMIN+9 waybar
+
 # Sway
 ~/.config/sway/darkmode.sh dark
--- darkman/.local/share/light-mode.d/light.sh
+++ darkman/.local/share/light-mode.d/light.sh
@@ -7,5 +7,8 @@ sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "Default Light
 # GTK
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
 
+# notify waybar module
+pkill -RTMIN+9 waybar
+
 # Sway
 ~/.config/sway/darkmode.sh light
